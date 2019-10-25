71239e930cf8f95f15d931fb9a3bc672e549c430
seanng <shonum@gmail.com>
2019-10-25 14:45:28 +0800

    move scraper to nested folder

---
+++ regulations.gov/initial/index.js
@@ -0,0 +1,40 @@
+/* eslint no-await-in-loop: 0 */
+/* eslint no-plusplus: 0 */
+/* eslint no-underscore-dangle: 0 */
+
+const puppeteer = require('puppeteer');
+const util = require('util');
+const path = require('path');
+const scrape = require('./scrape');
+const exec = util.promisify(require('child_process').exec);
+
+(async () => {
+  const { stdout } = await exec(`ls ${path.resolve(__dirname, 'downloads')} | wc -l`);
+  const idx = stdout * 1;
+
+  async function init() {
+    const browser = await puppeteer.launch({
+      args: [
+        '--incognito',
+        '--ignore-certificate-errors',
+        '--ignore-certificate-errors-spki-list ',
+      ],
+      // 300 does not work.
+      slowMo: 400,
+    });
+    const page = await browser.newPage();
+    try {
+      await scrape(page, idx);
+      await browser.close();
+    } catch (error) {
+      // LOG ERRORS
+      console.error('error: ', error);
+      console.error('error.name: ', error.name);
+      console.error('error.message: ', error.message);
+      await browser.close();
+    }
+  }
+  console.log('Current total downloads: ', idx);
+  init();
+})();
+
---
+++ regulations.gov/initial/scrape.js
@@ -0,0 +1,88 @@
+/* eslint no-await-in-loop: 0 */
+/* eslint no-underscore-dangle: 0 */
+/* eslint no-plusplus: 0 */
+
+const fs = require('fs');
+const uuid = require('uuid/v1');
+const rimraf = require('rimraf');
+const mkdirp = require('mkdirp');
+const path = require('path');
+// allDocuments has 68091 rows.
+const allDocuments = require('./documents.json');
+
+const excelIconSelector = 'img[src="./images/fileicons/small/icon_xls.gif"]';
+
+async function waitForFileToDownload(downloadPath) {
+  let filename;
+  let counter = 0;
+
+  await new Promise((resolve) => {
+    const interval = setInterval(() => {
+      if (counter === 100) {
+        console.log('Took longer than 10 seconds to download.');
+        clearInterval(interval);
+        resolve();
+      }
+
+      if (filename && !filename.endsWith('.crdownload')) {
+        clearInterval(interval);
+        resolve();
+      }
+      counter++;
+      [filename] = fs.readdirSync(downloadPath);
+    }, 100);
+  });
+
+  return filename;
+}
+
+async function download(page, title) {
+  const nestedFolder = uuid();
+  const downloadPath = path.resolve(__dirname, 'downloads', nestedFolder);
+  mkdirp(downloadPath);
+  await page._client.send('Page.setDownloadBehavior', { behavior: 'allow', downloadPath });
+  await page.$eval(excelIconSelector, elem => elem.parentElement.click());
+  const filename = await waitForFileToDownload(downloadPath);
+  if (filename) {
+    const filePath = path.resolve(downloadPath, filename);
+    const newPath = path.resolve(__dirname, 'downloads', title);
+    fs.renameSync(filePath, newPath);
+    console.log('File downloaded to ', newPath);
+  } else {
+    console.log('Re-attempting.');
+  }
+  rimraf.sync(downloadPath);
+  return !!filename;
+}
+
+function getTitle(document) {
+  const titlePrefix = document['Document Title']
+    .slice(9).split('-')[0].trim().toUpperCase();
+  return `${titlePrefix}_${document['Document ID']}.xlsx`;
+}
+
+module.exports = async function scrape(page, idx) {
+  /**
+   * MAKE SURE downloads FOLDER IS EMPTY IF CHANGING START.
+   * NUMBERS MUST BE LESS THAN 68091
+   */
+
+  const START = 0;
+  const END = 68091;
+
+  const documents = allDocuments.slice(START, END);
+  let newIdx = idx;
+
+  while (newIdx < documents.length) {
+    const document = documents[newIdx];
+    const title = getTitle(document);
+
+    await page.goto(document.URL);
+    console.log(`Navigating to ${document.URL}`);
+    await page.waitForSelector(excelIconSelector, {
+      timeout: 18000,
+    });
+    const downloaded = await download(page, title);
+    newIdx = downloaded ? newIdx + 1 : newIdx;
+  }
+};
