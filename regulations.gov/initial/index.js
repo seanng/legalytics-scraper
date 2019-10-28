fb1e9f7c63e2c48211c8dc494b1284af9ac2de8f
seanng <shonum@gmail.com>
2019-10-28 11:56:32 +0800

    try again after 10 minutes

--- regulations.gov/initial/index.js
+++ regulations.gov/initial/index.js
@@ -8,33 +8,33 @@ const path = require('path');
 const scrape = require('./scrape');
 const exec = util.promisify(require('child_process').exec);
 
-(async () => {
+(async function init() {
   const { stdout } = await exec(`ls ${path.resolve(__dirname, 'downloads')} | wc -l`);
   const idx = stdout * 1;
-
-  async function init() {
-    const browser = await puppeteer.launch({
-      args: [
-        '--incognito',
-        '--ignore-certificate-errors',
-        '--ignore-certificate-errors-spki-list ',
-      ],
-      // 300 does not work.
-      slowMo: 500,
-    });
-    const page = await browser.newPage();
-    try {
-      await scrape(page, idx);
-      await browser.close();
-    } catch (error) {
-      // LOG ERRORS
-      console.error('error: ', error);
-      console.error('error.name: ', error.name);
-      console.error('error.message: ', error.message);
-      await browser.close();
+  console.log('Current total downloads: ', idx);
+  const browser = await puppeteer.launch({
+    args: [
+      '--incognito',
+      '--ignore-certificate-errors',
+      '--ignore-certificate-errors-spki-list ',
+    ],
+    // 300 does not work.
+    slowMo: 500,
+    // headless: false,
+  });
+  const page = await browser.newPage();
+  try {
+    await scrape(page, idx);
+    await browser.close();
+  } catch (error) {
+    // LOG ERRORS
+    console.error('error: ', error);
+    console.error('error.name: ', error.name);
+    console.error('error.message: ', error.message);
+    await browser.close();
+    if (error.name === 'TimeoutError') {
+      setTimeout(init, 1000 * 60 * 10);
     }
   }
-  console.log('Current total downloads: ', idx);
-  init();
-})();
+}());
 
